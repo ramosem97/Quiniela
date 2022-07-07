123d8b71ce7312298785eee74896e9ddd00789ca
Emilio Ramos <ramosem97>
2022-07-07 08:26:09 -0700

    logging

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 import dash_bootstrap_components as dbc
 import pandas as pd
 import numpy as np
-# import nflapi
+import nflapi
 
 ############# GLOBAL VARS ###################
 USER_LIST = ['Gel','Hector','Emilio','Sonny']
-# nfl = nflapi.NFL(ua="nflapi_quiniela")
+nfl = nflapi.NFL(ua="nflapi_quiniela")
 
 HOME_COLS = {}
 HOME_COLS['id'] = ['home_team', 'abbreviation_home', 'nick_name_home',
@@ -133,3 +133,8 @@ app.config.suppress_callback_exceptions = True
 # app.scripts.config.serve_locally = True
 
 
+############## LOGGING #####################
+import logging, sys
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
